59977ed990006a00767fe11da49cf1e88238c1f2
unknown <robert.kolski@outlook.com>
2018-04-15 02:51:29 -0700

    Polyglot Parallel working for King James Version and Spanish Reina Valera

---
+++ polyglot/polyglot_parallel/LayoutHelper.cs
@@ -0,0 +1,242 @@
+﻿using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polyglot_parallel
+{
+    public class LayoutHelper
+    {
+        private readonly PdfDocument _document;
+        private readonly XUnit _topPosition;
+        private readonly XUnit _bottomMargin;
+        private readonly XUnit[] _leftMargin;
+        private readonly XUnit[] _rightMargin;
+        private XUnit _currentPosition;
+
+        public LayoutHelper(
+            PdfDocument document, 
+            XUnit topPosition, 
+            XUnit bottomMargin,
+            XUnit[] leftMargin,
+            XUnit[] rightMargin)
+        {
+            _document = document;
+            _topPosition = topPosition;
+            _bottomMargin = bottomMargin;
+            _currentPosition = _topPosition;
+            _leftMargin = leftMargin;
+            _rightMargin = rightMargin;
+            // Set a value outside the page - a new page will be created on the first request.
+            //bottomMargin + 10000;
+            CreatePage();
+        }
+
+        public Tuple<XUnit, XGraphics> GetLinePosition(XUnit requestedHeight)
+        {
+            return GetLinePosition(requestedHeight, -1f);
+        }
+
+        public Tuple<XUnit, XGraphics> GetLinePosition(XUnit requestedHeight, XUnit requiredHeight)
+        {
+            XUnit required = requiredHeight == -1f ? requestedHeight : requiredHeight;
+            if (_currentPosition + required > _bottomMargin)
+                CreatePage();
+            XUnit result = _currentPosition;
+            _currentPosition += requestedHeight;
+            return Tuple.Create(result, this.Gfx);
+        }
+
+        public string[,] GetLines(XUnit leftIndent, string[] testFit, XFont font)
+        {
+            bool doesNotFit = false;
+            for (int i = 0; i < testFit.Length; i++)
+            {
+                XSize sizeTestFit = Gfx.MeasureString(testFit[i], font);
+                double targetWidth = _rightMargin[i] - (_leftMargin[i] + leftIndent);
+                if (sizeTestFit.Width > targetWidth)
+                {
+                    doesNotFit = true;
+                }
+            }
+            if (!doesNotFit)
+            {
+                string[,] temp = new string[testFit.Length, 1];
+                for (int i = 0; i <testFit.Length; i++)
+                {
+                    temp[i, 0] = testFit[i];
+                }
+                return temp;
+            }
+
+            List<string[]> stringLines = new List<string[]>();
+            for (int i = 0; i < testFit.Length; i++)
+            {
+                XSize sizeTestFit = Gfx.MeasureString(testFit[i], font);
+                double targetWidth = _rightMargin[i] - (_leftMargin[i] + leftIndent);
+                if (sizeTestFit.Width > targetWidth)
+                {
+                    bool done = false;
+                    int j = testFit.Length;
+                    string shorter = testFit[i];
+                    string spill = testFit[i];
+                    int k = 0;
+                    while (!done)
+                    {
+                        XSize smallestNonFitting = Gfx.MeasureString(shorter, font);
+                        if (smallestNonFitting.Width < targetWidth)
+                        {
+                            // this means that contrary to variable
+                            // name it does fit
+                            if (i == 0)
+                            {
+                                string[] temp = new string[testFit.Length];
+                                temp[0] = shorter.Trim();
+                                stringLines.Add(temp);
+                            }
+                            else
+                            {
+                                if (stringLines.Count <= k)
+                                {
+                                    stringLines.Add(new string[testFit.Length]);
+                                }
+                                stringLines[k][i] = shorter.Trim();
+                            }
+                            done = true;
+                            break;
+                        }
+                        j = shorter.LastIndexOf(' ');
+                        string newShorter = shorter.Substring(0, j);
+                        XSize largestFitting = Gfx.MeasureString(newShorter, font);
+                        if (smallestNonFitting.Width > targetWidth &&
+                            largestFitting.Width <= targetWidth)
+                        {
+                            spill = spill.Substring(newShorter.Length);
+                            shorter = spill;
+                            if (i == 0)
+                            {
+                                string[] temp = new string[testFit.Length];
+                                temp[0] = newShorter.Trim();
+                                stringLines.Add(temp);
+                            }
+                            else
+                            {
+                                if (stringLines.Count <= k)
+                                {
+                                    stringLines.Add(new string[testFit.Length]);
+                                }
+                                stringLines[k][i] = newShorter.Trim();
+                                k++;
+                            }
+
+                        }
+                        else
+                        {
+                            shorter = newShorter;
+                        }
+
+                        if (string.IsNullOrWhiteSpace(shorter))
+                        {
+                            done = true;
+                        }
+                    }
+                }
+            }
+            string[,] result = new string[testFit.Length, stringLines.Count];
+            for (int i = 0; i < testFit.Length; i++)
+            {
+                for (int j = 0; j < stringLines.Count; j++)
+                {
+                    result[i, j] = stringLines[j][i];
+                }
+            }
+            return result;
+        }
+
+        public XGraphics Gfx { get; private set; }
+        public PdfPage Page { get; private set; }
+
+        public void CreatePage()
+        {
+            Page = _document.AddPage();
+            Page.Size = PageSize.Letter;
+            Gfx = XGraphics.FromPdfPage(Page);
+            _currentPosition = _topPosition;
+        }
+
+        public void PrintCentered(string[] text, XFont font)
+        {
+            string[,] lines = this.GetLines(0, text, font);
+
+            Tuple<XUnit,XGraphics>[] lineTop = new Tuple<XUnit, XGraphics>[lines.GetLength(1)];
+
+            for (int i = 0; i < lines.GetLength(0); i++)
+            {
+                for (int j = 0; j < lines.GetLength(1); j++)
+                {
+                    XSize size = this.Gfx.MeasureString(lines[i,j] ?? "A", font);
+                    if (i == 0)
+                    {
+                        lineTop[j] = this.GetLinePosition(size.Height);
+                    }
+                    XUnit leftIndentByCentering = (this._rightMargin[i] - this._leftMargin[i] - size.Width) / 2d;
+                    XPoint p = new XPoint(this._leftMargin[i] + leftIndentByCentering, lineTop[j].Item1);
+                    if (lines[i, j] != null)
+                    {
+                        lineTop[j].Item2.DrawString(lines[i, j], font, XBrushes.Black, p);
+                    }
+                }
+            }
+        }
+
+        public void PrintChapter(int chapter, XFont font)
+        {
+            Tuple<XUnit, XGraphics> lineTop = new Tuple<XUnit, XGraphics>(default(XUnit), null);
+            for (int i = 0; i < this._leftMargin.Length; i++)
+            {
+                string text = string.Format("Chapter {0}", chapter);
+                XSize size = this.Gfx.MeasureString(text, font);
+                if (i == 0)
+                {
+                    lineTop = this.GetLinePosition(size.Height);
+                }
+                XPoint p = new XPoint(this._leftMargin[i], lineTop.Item1);
+                lineTop.Item2.DrawString(text, font, XBrushes.Black, p);
+            }
+        }
+
+        public void PrintVerse(int verse, XUnit indentLeft, XFont font, string[] text)
+        {
+            string[,] lines = this.GetLines(indentLeft, text, font);
+
+            Tuple<XUnit,XGraphics>[] lineTop = new Tuple<XUnit,XGraphics>[lines.GetLength(1)];
+
+            for (int i = 0; i < lines.GetLength(0); i++)
+            {
+                for (int j = 0; j < lines.GetLength(1); j++)
+                {
+                    XSize size = this.Gfx.MeasureString(lines[i, j] ?? "A", font);
+                    if (i == 0)
+                    {
+                        lineTop[j] = this.GetLinePosition(size.Height);
+                    }
+                    if (j == 0)
+                    {
+                        XPoint np = new XPoint(this._leftMargin[i], lineTop[j].Item1);
+                        lineTop[j].Item2.DrawString(string.Format("{0}.", verse), font, XBrushes.Black, np);
+                    }
+
+                    XPoint p = new XPoint(this._leftMargin[i] + indentLeft, lineTop[j].Item1);
+                    if (lines[i, j] != null)
+                    {
+                        lineTop[j].Item2.DrawString(lines[i, j], font, XBrushes.Black, p);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ polyglot/polyglot_parallel/Program.cs
@@ -0,0 +1,84 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace polyglot_parallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                System.Console.Out.WriteLine("usage: polyglot_pdfsharp filename1 filename2 outfile");
+                return;
+            }
+
+            string filename1 = args[0];
+            string filename2 = args[1];
+            string outfile = args[2];
+
+            PdfDocument doc = new PdfDocument();
+            XUnit[] leftMargin = new XUnit[] { XUnit.FromInch(1.0), XUnit.FromInch(4.5) };
+            XUnit[] rightMargin = new XUnit[] { XUnit.FromInch(4.0), XUnit.FromInch(7.5) };
+            LayoutHelper layoutHelper = new LayoutHelper(doc, XUnit.FromInch(1.0), XUnit.FromInch(10.0), leftMargin, rightMargin);
+
+            XFont font = new XFont("Verdana", 12);
+            XUnit indentLeft = XUnit.FromInch(.5);
+
+            XmlDocument xml = new XmlDocument();
+            xml.Load(filename1);
+            XmlDocument xml2 = new XmlDocument();
+            xml2.Load(filename2);
+            foreach (XmlNode bookNode in xml.SelectNodes(@"//BIBLEBOOK"))
+            {
+                int bookNumber = int.Parse(bookNode.Attributes["bnumber"].Value);
+                string bookName = bookNode.Attributes["bname"].Value;
+                string[] bookArray = new string[2];
+                bookArray[0] = bookName;
+
+                XmlNode bookNode2 = xml2.SelectSingleNode(string.Format(@"//BIBLEBOOK[@bnumber='{0}']", bookNumber));
+
+                bookArray[1] = bookNode2.Attributes["bname"].Value;
+
+                layoutHelper.PrintCentered(bookArray, font);
+                foreach (XmlNode chapterNode in bookNode.SelectNodes("CHAPTER"))
+                {
+                    int chapterNumber = int.Parse(chapterNode.Attributes["cnumber"].Value);
+
+                    XmlNode chapterNode2 = bookNode2.SelectSingleNode(string.Format("CHAPTER[@cnumber='{0}']", chapterNumber));
+
+                    layoutHelper.PrintChapter(chapterNumber, font);
+                    foreach (XmlNode verseNode in chapterNode.SelectNodes("VERS"))
+                    {
+                        int verseNumber = int.Parse(verseNode.Attributes["vnumber"].Value);
+                        string verseText = verseNode.InnerText;
+                        string[] verseArray = new string[2];
+                        verseArray[0] = verseText;
+
+                        XmlNode verseNode2 = chapterNode2.SelectSingleNode(string.Format("VERS[@vnumber='{0}']", verseNumber));
+
+                        verseArray[1] = verseNode2.InnerText;
+
+                        layoutHelper.PrintVerse(verseNumber, indentLeft, font, verseArray);
+
+                        //if (bookNumber >= 5 && chapterNumber >= 5 && verseNumber >= 10)
+                        //{
+                        //    goto test;
+                        //}
+                    }
+                }
+            }
+
+            //test:
+
+            // Save the document... 
+            doc.Save(outfile);
+        }
+    }
+}
